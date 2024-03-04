dd98cfc4451b20b0757766573e8d205956493fb7
GuilhermeDaviNascimento <131400280+GuilhermeDaviNascimento@users.noreply.github.com>
2024-03-04 10:55:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/delete', (req, res) => {
 });
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
